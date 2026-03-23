660c0b1f1f3865ada701c7745341ac2b43685381
Richardpollo66? <richard.1530@hotmail.com>
2026-03-23 02:11:03 -0600

    fix env projectId

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
+import React from 'react';
+import ReactDOM from "react-dom/client";
 import '@rainbow-me/rainbowkit/styles.css';
 import { WagmiProvider } from "wagmi";
 import { config } from "./lib/config";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-
+import App from './App.jsx';
 
 const queryClient = new QueryClient();
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
 <WagmiProvider config={config}>
     <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
@@ -14,3 +17,4 @@ const queryClient = new QueryClient();
         </RainbowKitProvider>
     </QueryClientProvider>
 </WagmiProvider>
+</React.StrictMode>);
