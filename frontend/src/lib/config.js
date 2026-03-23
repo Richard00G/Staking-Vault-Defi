d70557540771e7652c63d67dac4b9e831e505178
Richardpollo66? <richard.1530@hotmail.com>
2026-03-23 03:32:09 -0600

    UI upgrade

--- frontend/src/lib/config.js
+++ frontend/src/lib/config.js
@@ -3,7 +3,7 @@ import { sepolia } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
     appName: 'Staking Vault ',
-    projectId: 'CMdCcd6P8CLzdDGdHRActaZvab17KZpcByJmWn7SrSdB',
+    
    // projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
     chains:[sepolia],
     ssr: false,
